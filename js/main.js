7ce35229bd76e2540f0372be7611db69f52254b5
EstebanG93 <s.garciam3626@gmail.com>
2022-08-14 00:18:40 -0500

    objetos creados y funcionales

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//              Variables de proceso
 let prod=0;
 let pay = 0;
 let estado=0;
@@ -6,18 +7,32 @@ let cuenta_tax=0;
 let cantidad = 0;
 let cantidad_total=0;
 let med_pago = 0;
-const productos =   ["Sandwich","Ensalada","Hamburguesa","Hot Dog","Coca-Cola","Pepsi","Fanta"];
-const precios   =   [3.5,       4.5,        6.5,            4.99,       3,      2.8,    2.5];
-const resumen_qty = [];
+const items = [];
+const productos =   [];
+const tipos = [];
+const precios = [];
+const resumen_product=[];
+const resumen_type = []
 const resumen_price = [];
-const historial=[];
+const resumen_qty = [];
 
+//                  Funciones flecha con operaciones básicas
 const suma = (a,b) => a+b;
 const resta = (a,b) => a-b;
 const multi = (a,b) => a*b;
 const division = (a,b) => a/b;
 const taxes = a => a*0.21;
 
+//                  Objeto con todo el protafolio de productos
+const menu = [  {item: 1, producto: "Sandwich", tipo:"comida", precio: 3.5},
+                {item: 2, producto: "Ensalada", tipo:"comida", precio: 4.5},
+                {item: 3, producto: "Hamburguesa", tipo: "comida", precio: 6.5},
+                {item: 4, producto: "Hot Dog", tipo:"comida", precio:4.99},
+                {item: 5, producto: "Coca-Cola", tipo: "bebida", precio: 3},
+                {item: 6, producto: "Pepsi", tipo: "bebida", precio: 2.8},
+                {item: 7, producto: "Fanta", tipo: "bebida", precio: 2.5}];
+
+//                  Función para verificar el estado del pedido
 function Estado(){
     estado = parseFloat(prompt("Ingrese la opción deseada:\n\n1. Añadir producto.\n2. Ir a pagar."));
 
@@ -27,6 +42,7 @@ function Estado(){
     }
 }
 
+//                  Función para añadir producto
 function AddProduct (){
     prod = parseFloat(prompt("Por favor indique el producto que desea:\n\n1. Sandwich: USD 3.5 c/u\n2. Ensalada: USD 4.5\n3. Hamburguesa: USD 6.5\n4. Hot Dog: USD 4.99\n5. Coca-Cola: USD 3\n6. Pepsi: USD 2.8\n7. Fanta: USD 2.5"));
 
@@ -35,8 +51,10 @@ function AddProduct (){
         prod = parseFloat(prompt("Por favor indique el producto que desea:\n\n1. Sandwich: USD 3.5 c/u\n2. Ensalada: USD 4.5\n3. Hamburguesa: USD 6.5\n4. Hot Dog: USD 4.99\n5. Coca-Cola: USD 3\n6. Pepsi: USD 2.8\n7. Fanta: USD 2.5"));
     }
 
-    console.log("Producto: "+productos[prod-1]);
-    pay=precios[prod-1];
+    let item_actual = items.indexOf(prod);
+    let producto_actual = productos[item_actual];
+    let tipo_actual = tipos[item_actual]
+    pay=precios[item_actual];
 
     cantidad=parseFloat(prompt("Ingrese la cantidad solicitada:"));
 
@@ -45,13 +63,13 @@ function AddProduct (){
         cantidad=parseFloat(prompt("Ingrese la cantidad solicitada:"));
     }
 
-    console.log("Cantidad:",cantidad);
-
+    resumen_product.push(producto_actual);
+    resumen_type.push(tipo_actual);
     resumen_price.push(pay);
     resumen_qty.push(cantidad);
-    historial.push(productos[prod-1]);
 }
 
+//                  Función para indicar medio de pago
 function Payment(method){
     let cuotas=0;
     let valor_cuota=0;
@@ -72,6 +90,14 @@ function Payment(method){
 alert("Bienvenido!");
 Estado();
 
+//                  Creación de arrays de cada característica de los objetos
+for(const catalogo of menu){
+    items.push(catalogo.item);
+    productos.push(catalogo.producto);
+    tipos.push(catalogo.tipo);
+    precios.push(catalogo.precio);
+}
+
 while(estado!=2){
     AddProduct();
     Estado();
@@ -79,8 +105,8 @@ while(estado!=2){
 
 console.log("Pedido confirmado:")
 
-for(let i=0; i<resumen_qty.length; i++){
-    console.log(historial[i]+" cantidad: "+resumen_qty[i]);
+for(let i=0; i<resumen_product.length; i++){
+    console.log(resumen_product[i]+" cantidad: "+resumen_qty[i]);
     cantidad_total=suma(cantidad_total,resumen_qty[i]);
     subto=suma(subto,multi(resumen_price[i],resumen_qty[i]));
     cuenta_tax=taxes(subto);
@@ -96,3 +122,8 @@ while(med_pago%1!=0 || med_pago>2 || med_pago<1 || isNaN(med_pago)){
 }
 
 Payment(med_pago);
+
+console.log(resumen_product);
+console.log(resumen_type);
+console.log(resumen_price);
+console.log(resumen_qty);
